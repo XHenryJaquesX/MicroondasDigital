700842f0070965541670e8211128b5cfa411660e
Henry Jaques <iphonehenry@outlook.com>
2024-03-20 15:52:57 -0300

    Adicionar arquivos de projeto.

---
+++ MicroondasDigital/Form1.Designer.cs
@@ -0,0 +1,338 @@
+﻿namespace MicroondasDigital
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.txtTempo = new System.Windows.Forms.TextBox();
+            this.txtPotencia = new System.Windows.Forms.TextBox();
+            this.btnIniciarPausar = new System.Windows.Forms.Button();
+            this.btnCancelarLimpar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnNum1 = new System.Windows.Forms.Button();
+            this.btnNum2 = new System.Windows.Forms.Button();
+            this.btnNum3 = new System.Windows.Forms.Button();
+            this.btnNum4 = new System.Windows.Forms.Button();
+            this.btnNum5 = new System.Windows.Forms.Button();
+            this.btnNum6 = new System.Windows.Forms.Button();
+            this.btnNum7 = new System.Windows.Forms.Button();
+            this.btnNum8 = new System.Windows.Forms.Button();
+            this.btnNum9 = new System.Windows.Forms.Button();
+            this.btnNum0 = new System.Windows.Forms.Button();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnPotencia = new System.Windows.Forms.Button();
+            this.btnPipoca = new System.Windows.Forms.Button();
+            this.btnLeite = new System.Windows.Forms.Button();
+            this.btnCarne = new System.Windows.Forms.Button();
+            this.btnFrango = new System.Windows.Forms.Button();
+            this.btnFeijao = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtTempo
+            // 
+            this.txtTempo.Location = new System.Drawing.Point(694, 36);
+            this.txtTempo.Name = "txtTempo";
+            this.txtTempo.Size = new System.Drawing.Size(267, 20);
+            this.txtTempo.TabIndex = 0;
+            this.txtTempo.TextChanged += new System.EventHandler(this.txtTempo_TextChanged);
+            // 
+            // txtPotencia
+            // 
+            this.txtPotencia.Location = new System.Drawing.Point(694, 62);
+            this.txtPotencia.Name = "txtPotencia";
+            this.txtPotencia.Size = new System.Drawing.Size(266, 20);
+            this.txtPotencia.TabIndex = 1;
+            // 
+            // btnIniciarPausar
+            // 
+            this.btnIniciarPausar.Location = new System.Drawing.Point(673, 423);
+            this.btnIniciarPausar.Name = "btnIniciarPausar";
+            this.btnIniciarPausar.Size = new System.Drawing.Size(115, 53);
+            this.btnIniciarPausar.TabIndex = 2;
+            this.btnIniciarPausar.Text = "Iniciar / +30sec";
+            this.btnIniciarPausar.UseVisualStyleBackColor = true;
+            this.btnIniciarPausar.Click += new System.EventHandler(this.btnIniciarPausar_Click);
+            // 
+            // btnCancelarLimpar
+            // 
+            this.btnCancelarLimpar.Location = new System.Drawing.Point(830, 423);
+            this.btnCancelarLimpar.Name = "btnCancelarLimpar";
+            this.btnCancelarLimpar.Size = new System.Drawing.Size(115, 53);
+            this.btnCancelarLimpar.TabIndex = 3;
+            this.btnCancelarLimpar.Text = "Pausar / Cancelar";
+            this.btnCancelarLimpar.UseVisualStyleBackColor = true;
+            this.btnCancelarLimpar.Click += new System.EventHandler(this.btnCancelarLimpar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(645, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Tempo:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // btnNum1
+            // 
+            this.btnNum1.Location = new System.Drawing.Point(694, 142);
+            this.btnNum1.Name = "btnNum1";
+            this.btnNum1.Size = new System.Drawing.Size(75, 48);
+            this.btnNum1.TabIndex = 5;
+            this.btnNum1.Text = "1";
+            this.btnNum1.UseVisualStyleBackColor = true;
+            this.btnNum1.Click += new System.EventHandler(this.btnNum1_Click);
+            // 
+            // btnNum2
+            // 
+            this.btnNum2.Location = new System.Drawing.Point(790, 142);
+            this.btnNum2.Name = "btnNum2";
+            this.btnNum2.Size = new System.Drawing.Size(75, 48);
+            this.btnNum2.TabIndex = 6;
+            this.btnNum2.Text = "2";
+            this.btnNum2.UseVisualStyleBackColor = true;
+            this.btnNum2.Click += new System.EventHandler(this.btnNum2_Click);
+            // 
+            // btnNum3
+            // 
+            this.btnNum3.Location = new System.Drawing.Point(886, 142);
+            this.btnNum3.Name = "btnNum3";
+            this.btnNum3.Size = new System.Drawing.Size(75, 48);
+            this.btnNum3.TabIndex = 7;
+            this.btnNum3.Text = "3";
+            this.btnNum3.UseVisualStyleBackColor = true;
+            this.btnNum3.Click += new System.EventHandler(this.btnNum3_Click);
+            // 
+            // btnNum4
+            // 
+            this.btnNum4.Location = new System.Drawing.Point(694, 216);
+            this.btnNum4.Name = "btnNum4";
+            this.btnNum4.Size = new System.Drawing.Size(75, 48);
+            this.btnNum4.TabIndex = 8;
+            this.btnNum4.Text = "4";
+            this.btnNum4.UseVisualStyleBackColor = true;
+            this.btnNum4.Click += new System.EventHandler(this.btnNum4_Click);
+            // 
+            // btnNum5
+            // 
+            this.btnNum5.Location = new System.Drawing.Point(790, 216);
+            this.btnNum5.Name = "btnNum5";
+            this.btnNum5.Size = new System.Drawing.Size(75, 48);
+            this.btnNum5.TabIndex = 9;
+            this.btnNum5.Text = "5";
+            this.btnNum5.UseVisualStyleBackColor = true;
+            this.btnNum5.Click += new System.EventHandler(this.btnNum5_Click);
+            // 
+            // btnNum6
+            // 
+            this.btnNum6.Location = new System.Drawing.Point(886, 216);
+            this.btnNum6.Name = "btnNum6";
+            this.btnNum6.Size = new System.Drawing.Size(75, 48);
+            this.btnNum6.TabIndex = 10;
+            this.btnNum6.Text = "6";
+            this.btnNum6.UseVisualStyleBackColor = true;
+            this.btnNum6.Click += new System.EventHandler(this.btnNum6_Click);
+            // 
+            // btnNum7
+            // 
+            this.btnNum7.Location = new System.Drawing.Point(694, 288);
+            this.btnNum7.Name = "btnNum7";
+            this.btnNum7.Size = new System.Drawing.Size(75, 49);
+            this.btnNum7.TabIndex = 11;
+            this.btnNum7.Text = "7";
+            this.btnNum7.UseVisualStyleBackColor = true;
+            this.btnNum7.Click += new System.EventHandler(this.btnNum7_Click);
+            // 
+            // btnNum8
+            // 
+            this.btnNum8.Location = new System.Drawing.Point(790, 288);
+            this.btnNum8.Name = "btnNum8";
+            this.btnNum8.Size = new System.Drawing.Size(75, 48);
+            this.btnNum8.TabIndex = 12;
+            this.btnNum8.Text = "8";
+            this.btnNum8.UseVisualStyleBackColor = true;
+            this.btnNum8.Click += new System.EventHandler(this.btnNum8_Click);
+            // 
+            // btnNum9
+            // 
+            this.btnNum9.Location = new System.Drawing.Point(887, 288);
+            this.btnNum9.Name = "btnNum9";
+            this.btnNum9.Size = new System.Drawing.Size(75, 49);
+            this.btnNum9.TabIndex = 13;
+            this.btnNum9.Text = "9";
+            this.btnNum9.UseVisualStyleBackColor = true;
+            this.btnNum9.Click += new System.EventHandler(this.btnNum9_Click);
+            // 
+            // btnNum0
+            // 
+            this.btnNum0.Location = new System.Drawing.Point(790, 354);
+            this.btnNum0.Name = "btnNum0";
+            this.btnNum0.Size = new System.Drawing.Size(75, 52);
+            this.btnNum0.TabIndex = 14;
+            this.btnNum0.Text = "0";
+            this.btnNum0.UseVisualStyleBackColor = true;
+            this.btnNum0.Click += new System.EventHandler(this.btnNum0_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(636, 65);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 13);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "Potência:";
+            // 
+            // btnPotencia
+            // 
+            this.btnPotencia.Location = new System.Drawing.Point(694, 354);
+            this.btnPotencia.Name = "btnPotencia";
+            this.btnPotencia.Size = new System.Drawing.Size(75, 52);
+            this.btnPotencia.TabIndex = 17;
+            this.btnPotencia.Text = "Potência";
+            this.btnPotencia.UseVisualStyleBackColor = true;
+            this.btnPotencia.Click += new System.EventHandler(this.btnPotencia_Click);
+            // 
+            // btnPipoca
+            // 
+            this.btnPipoca.Location = new System.Drawing.Point(885, 354);
+            this.btnPipoca.Name = "btnPipoca";
+            this.btnPipoca.Size = new System.Drawing.Size(75, 52);
+            this.btnPipoca.TabIndex = 18;
+            this.btnPipoca.Text = "Pipoca";
+            this.btnPipoca.UseVisualStyleBackColor = true;
+            this.btnPipoca.Click += new System.EventHandler(this.btnPipoca_Click);
+            // 
+            // btnLeite
+            // 
+            this.btnLeite.Location = new System.Drawing.Point(613, 216);
+            this.btnLeite.Name = "btnLeite";
+            this.btnLeite.Size = new System.Drawing.Size(75, 48);
+            this.btnLeite.TabIndex = 19;
+            this.btnLeite.Text = "Leite";
+            this.btnLeite.UseVisualStyleBackColor = true;
+            this.btnLeite.Click += new System.EventHandler(this.btnLeite_Click);
+            // 
+            // btnCarne
+            // 
+            this.btnCarne.Location = new System.Drawing.Point(613, 288);
+            this.btnCarne.Name = "btnCarne";
+            this.btnCarne.Size = new System.Drawing.Size(75, 48);
+            this.btnCarne.TabIndex = 20;
+            this.btnCarne.Text = "Carnes de boi";
+            this.btnCarne.UseVisualStyleBackColor = true;
+            this.btnCarne.Click += new System.EventHandler(this.btnCarne_Click);
+            // 
+            // btnFrango
+            // 
+            this.btnFrango.Location = new System.Drawing.Point(613, 354);
+            this.btnFrango.Name = "btnFrango";
+            this.btnFrango.Size = new System.Drawing.Size(75, 52);
+            this.btnFrango.TabIndex = 21;
+            this.btnFrango.Text = "Frango";
+            this.btnFrango.UseVisualStyleBackColor = true;
+            this.btnFrango.Click += new System.EventHandler(this.btnFrango_Click);
+            // 
+            // btnFeijao
+            // 
+            this.btnFeijao.Location = new System.Drawing.Point(613, 142);
+            this.btnFeijao.Name = "btnFeijao";
+            this.btnFeijao.Size = new System.Drawing.Size(75, 48);
+            this.btnFeijao.TabIndex = 22;
+            this.btnFeijao.Text = "Feijão";
+            this.btnFeijao.UseVisualStyleBackColor = true;
+            this.btnFeijao.Click += new System.EventHandler(this.btnFeijao_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1072, 542);
+            this.Controls.Add(this.btnFeijao);
+            this.Controls.Add(this.btnFrango);
+            this.Controls.Add(this.btnCarne);
+            this.Controls.Add(this.btnLeite);
+            this.Controls.Add(this.btnPipoca);
+            this.Controls.Add(this.btnPotencia);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnNum0);
+            this.Controls.Add(this.btnNum9);
+            this.Controls.Add(this.btnNum8);
+            this.Controls.Add(this.btnNum7);
+            this.Controls.Add(this.btnNum6);
+            this.Controls.Add(this.btnNum5);
+            this.Controls.Add(this.btnNum4);
+            this.Controls.Add(this.btnNum3);
+            this.Controls.Add(this.btnNum2);
+            this.Controls.Add(this.btnNum1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnCancelarLimpar);
+            this.Controls.Add(this.btnIniciarPausar);
+            this.Controls.Add(this.txtPotencia);
+            this.Controls.Add(this.txtTempo);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtTempo;
+        private System.Windows.Forms.TextBox txtPotencia;
+        private System.Windows.Forms.Button btnIniciarPausar;
+        private System.Windows.Forms.Button btnCancelarLimpar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnNum1;
+        private System.Windows.Forms.Button btnNum2;
+        private System.Windows.Forms.Button btnNum3;
+        private System.Windows.Forms.Button btnNum4;
+        private System.Windows.Forms.Button btnNum5;
+        private System.Windows.Forms.Button btnNum6;
+        private System.Windows.Forms.Button btnNum7;
+        private System.Windows.Forms.Button btnNum8;
+        private System.Windows.Forms.Button btnNum9;
+        private System.Windows.Forms.Button btnNum0;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnPotencia;
+        private System.Windows.Forms.Button btnPipoca;
+        private System.Windows.Forms.Button btnLeite;
+        private System.Windows.Forms.Button btnCarne;
+        private System.Windows.Forms.Button btnFrango;
+        private System.Windows.Forms.Button btnFeijao;
+    }
+}
+
---
+++ MicroondasDigital/Form1.cs
@@ -0,0 +1,299 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace MicroondasDigital
+{
+    public partial class Form1 : Form
+    {
+        private Timer timerAquecimento = new Timer();
+        private int tempoRestante;
+        public Form1()
+        {
+            InitializeComponent();
+            timerAquecimento.Interval = 1000;
+            timerAquecimento.Tick += TimerAquecimento_Tick;
+        }
+        private bool aquecendo = false;
+        private bool isPaused = false;
+        private void TimerAquecimento_Tick(object sender, EventArgs e)
+        {
+            if (tempoRestante > 0)
+            {
+                tempoRestante--;
+                txtTempo.Text = $"{tempoRestante / 60:D2}:{tempoRestante % 60:D2}";
+            }
+            else
+            {
+                timerAquecimento.Stop();
+                MessageBox.Show("Aquecimento concluído!");
+                aquecendo = false;
+            }
+        }
+
+        private void IniciarAquecimento(int tempo, int potencia)
+        {
+            MessageBox.Show($"Aquecendo por {tempo / 60} minutos e {tempo % 60} segundos na potência {potencia}.");
+        }
+
+        private void txtTempo_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnNum1_Click(object sender, EventArgs e)
+        {
+            AdicionarNumeroAoTempo("1");
+        }
+
+        private void btnNum2_Click(object sender, EventArgs e)
+        {
+            AdicionarNumeroAoTempo("2");
+        }
+
+        private void btnNum3_Click(object sender, EventArgs e)
+        {
+            AdicionarNumeroAoTempo("3");
+        }
+
+        private void btnNum4_Click(object sender, EventArgs e)
+        {
+            AdicionarNumeroAoTempo("4");
+        }
+
+        private void btnNum5_Click(object sender, EventArgs e)
+        {
+            AdicionarNumeroAoTempo("5");
+        }
+
+        private void btnNum6_Click(object sender, EventArgs e)
+        {
+            AdicionarNumeroAoTempo("6");
+        }
+
+        private void btnNum7_Click(object sender, EventArgs e)
+        {
+            AdicionarNumeroAoTempo("7");
+        }
+
+        private void btnNum8_Click(object sender, EventArgs e)
+        {
+            AdicionarNumeroAoTempo("8");
+        }
+
+        private void btnNum9_Click(object sender, EventArgs e)
+        {
+            AdicionarNumeroAoTempo("9");
+        }
+
+        private void btnNum0_Click(object sender, EventArgs e)
+        {
+            AdicionarNumeroAoTempo("0");
+        }
+
+        private void btnPotencia_Click(object sender, EventArgs e)
+        {
+            if(int.TryParse(txtPotencia.Text, out int potenciaAtual) && potenciaAtual > 1)
+            {
+                txtPotencia.Text = (potenciaAtual - 1).ToString();
+            }
+            else
+            {
+                txtPotencia.Text = "10";
+            }
+        }
+
+        private void btnIniciarPausar_Click(object sender, EventArgs e)
+        {
+            int potencia = 10;
+            if (!string.IsNullOrWhiteSpace(txtPotencia.Text) && int.TryParse(txtPotencia.Text, out int potenciaLida))
+            {
+                potencia = potenciaLida;
+            }
+
+            if (!aquecendo)
+            {
+                if (string.IsNullOrWhiteSpace(txtTempo.Text))
+                {
+                    tempoRestante = 30; 
+                }
+                else
+                {
+                    string textoTempo = txtTempo.Text.Replace(":", "");
+                    if (textoTempo.Length <= 4 && int.TryParse(textoTempo, out int totalSegundos))
+                    {
+                        ConvertAndSetTime(textoTempo);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Insira um tempo válido.");
+                        return;
+                    }
+                }
+
+                IniciarAquecimento(tempoRestante, potencia);
+                timerAquecimento.Start();
+                aquecendo = true;
+            }
+            else
+            {
+                tempoRestante += 30;
+                MessageBox.Show("30 segundos adicionados ao tempo de aquecimento.");
+            }
+        }
+
+        private void ConvertAndSetTime(string textoTempo)
+        {
+            if (textoTempo.Length <= 2)
+            {
+                tempoRestante = int.Parse(textoTempo);
+            }
+            else if (textoTempo.Length == 3)
+            {
+                tempoRestante = int.Parse(textoTempo[0].ToString()) * 60 + int.Parse(textoTempo.Substring(1));
+            }
+            else if (textoTempo.Length == 4)
+            {
+                tempoRestante = int.Parse(textoTempo.Substring(0, 2)) * 60 + int.Parse(textoTempo.Substring(2));
+            }
+        }
+
+
+
+        private void btnCancelarLimpar_Click(object sender, EventArgs e)
+        {
+            if (aquecendo)
+            {
+                timerAquecimento.Stop();
+                MessageBox.Show("Aquecimento pausado.");
+                aquecendo = false;
+                isPaused = true;
+            }
+            else if (isPaused)
+            {
+                txtTempo.Clear();
+                txtPotencia.Clear();
+                tempoRestante = 0;
+                isPaused = false;
+            }
+            else 
+            {
+                txtTempo.Clear();
+                txtPotencia.Clear();
+            }
+        }
+
+
+        private void AdicionarNumeroAoTempo(string numero)
+        {
+            string textoAtual = txtTempo.Text.Replace(":", "");
+
+            if (textoAtual.Length < 4)
+            {
+                textoAtual += numero;
+            }
+
+            if (textoAtual.Length > 2)
+            {
+                txtTempo.Text = textoAtual.Substring(0, textoAtual.Length - 2) + ":" + textoAtual.Substring(textoAtual.Length - 2);
+            }
+            else
+            {
+                txtTempo.Text = textoAtual;
+            }
+        }
+
+        private void btnFeijao_Click(object sender, EventArgs e)
+        {
+            if (!aquecendo)
+            {
+                SetAndStartAquecimento(480, 9);
+                MessageBox.Show("Deixe o recipiente destampado e em casos de plástico, cuidado ao retirar o recipiente pois o mesmo pode perder resistência em altas temperaturas.");
+            }
+            else
+            {
+                MessageBox.Show("O aquecimento já está em andamento. Por favor, aguarde ou cancele antes de iniciar um novo programa.");
+            }
+        }
+
+        private void btnLeite_Click(object sender, EventArgs e)
+        {
+            if (!aquecendo)
+            {
+                SetAndStartAquecimento(300, 5);
+                MessageBox.Show("Cuidado com aquecimento de líquidos, o choque térmico aliado ao movimento do recipiente pode causar fervura imediata causando risco de queimaduras.");
+            }
+            else
+            {
+                MessageBox.Show("O aquecimento já está em andamento. Por favor, aguarde ou cancele antes de iniciar um novo programa.");
+            }
+        }
+
+        private void btnCarne_Click(object sender, EventArgs e)
+        {
+            if (!aquecendo)
+            {
+                SetAndStartAquecimento(840, 4);
+                MessageBox.Show("Interrompa o processo na metade e vire o conteúdo com a parte de baixo para cima para o descongelamento uniforme.");
+            }
+            else
+            {
+                MessageBox.Show("O aquecimento já está em andamento. Por favor, aguarde ou cancele antes de iniciar um novo programa.");
+            }
+        }
+
+        private void btnFrango_Click(object sender, EventArgs e)
+        {
+            if (!aquecendo)
+            {
+                SetAndStartAquecimento(480, 7); 
+                MessageBox.Show("Interrompa o processo na metade e vire o conteúdo com a parte de baixo para cima para o descongelamento uniforme.");
+            }
+            else
+            {
+                MessageBox.Show("O aquecimento já está em andamento. Por favor, aguarde ou cancele antes de iniciar um novo programa.");
+            }
+        }
+
+        private void btnPipoca_Click(object sender, EventArgs e)
+        {
+            if (!aquecendo)
+            {
+                SetAndStartAquecimento(180, 7); 
+                MessageBox.Show("Observar o barulho de estouros do milho, caso houver um intervalo de mais de 10 segundos entre um estouro e outro, interrompa o aquecimento.");
+            }
+            else
+            {
+                MessageBox.Show("O aquecimento já está em andamento. Por favor, aguarde ou cancele antes de iniciar um novo programa.");
+            }
+        }
+
+        private void SetAndStartAquecimento(int tempo, int potencia)
+        {
+            if (aquecendo)
+            {
+                MessageBox.Show("O aquecimento já está em andamento. Por favor, aguarde ou cancele antes de iniciar um novo programa.");
+                return;
+            }
+
+            tempoRestante = tempo;
+            txtTempo.Text = $"{tempo / 60:D2}:{tempo % 60:D2}";
+            txtPotencia.Text = potencia.ToString();
+            timerAquecimento.Start();
+            aquecendo = true;
+            isPaused = false;
+
+            MessageBox.Show($"Aquecimento iniciado: {tempo / 60} minuto(s) e {tempo % 60} segundo(s) na potência {potencia}.");
+        }
+
+    }
+}
